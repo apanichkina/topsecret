b546d49ba673082cf29d609cd167ecfc33421964
anna <annjellyiu5@gmail.com>
2015-09-16 11:24:45 +0300

    menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,12 @@ module.exports = function(grunt) {
   
 grunt.initConfig({  
 
-concat: {  
-options: {
-	separator: ';' 
-},  
-foo: { 
-	options: { },
-	src: ['js/first.js', 'js/second.js'],
-  	dest: 'js/foo.js'
- },  
-bar: { /* Цель bar */ } 
-},
 
 shell: {
+    options: {
+                stdout: true,
+                stderr: true
+    },
 	server: { /* Подзадача */
     		command: 'java -cp L1.2-1.0-jar-with-dependencies.jar main.Main 8080'
 	}
@@ -24,15 +17,16 @@ shell: {
 fest: {
     templates: {
         files: [{
-            expand: true,
-            cwd: 'templates',
-            src: '*.xml',
-            dest: 'public_html/js/tmpl'
+            expand: true, // Флаг динамического развертывания
+            cwd: 'templates', /* исходная директория */
+            src: '*.xml', /* имена шаблонов */
+            dest: 'public_html/js/tmpl' /* результирующая директория */
         }],
         options: {
-            template: function (data) {
+            template: function (data) { /* задаем формат функции-шаблона */
                 return grunt.template.process(
-                    'var <%= name %>Tmpl = <%= contents %> ;',
+                    // 'define(function () { return <%= contents %> ; });',
+                    'var <%= name %>Tmpl = <%= contents %> ;', /* присваиваем функцию-шаблон переменной */
                     {data: data}
                 );
             }
@@ -44,12 +38,17 @@ watch: {
     	files: ['templates/*.xml'], /* следим за шаблонами */
     	tasks: ['fest'], /* перекомпилировать */
     	options: {
+            interrupt: true,
         	atBegin: true /* запустить задачу при старте */
     	}
 	},
 	server: { /* Цель */
-    	files: ['public_html/js/**/*.js'], /* следим за JS */
-    	options: {
+    	files: [
+            'public_html/js/**/*.js', /* следим за статикой */
+            'public_html/css/**/*.css'
+        ],     	
+        options: {
+            interrupt: true,
         	livereload: true /* автоматическая перезагрузка */
     	}
 	}
@@ -57,7 +56,7 @@ watch: {
 concurrent: {
     target: ['watch', 'shell'],
     options: {
-        logConcurrentOutput: true, /* Вывод процесса */
+        logConcurrentOutput: true, /* Вывод процесса логов*/
     }
 }, 
 any_other_name: 'hello' /* Любое произвольное свойство */
