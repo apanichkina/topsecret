69c6fbf2c88e07316c763a5c29a910d8f0e60981
Alex <s3ter3@gmail.com>
2015-11-20 15:24:02 +0300

    sass ready

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,71 +3,94 @@ module.exports = function(grunt) {
   
 grunt.initConfig({  
 
-
-shell: {
-    options: {
-        stdout: true,
-        stderr: true
-    },
-	server: {
-        command: 'java -cp 4pg-0.0.1-jar-with-dependencies.jar main.Main'
-	}
-},
-
-fest: {
-    templates: {
-        files: [{
-            expand: true, // Флаг динамического развертывания
-            cwd: 'templates', /* исходная директория */
-            src: '*.xml', /* имена шаблонов */
-            dest: 'public_html/js/tmpl' /* результирующая директория */
-        }],
+    shell: {
         options: {
-            template: function (data) {
-                return grunt.template.process(
-                    // 'var <%= name %>Tmpl = <%= contents %> ;',
-                    'define(function () { return <%= contents %> ; });',
-                    {data: data}
-                );
+            stdout: true,
+            stderr: true
+        },
+        server: {
+            command: 'java -cp 4pg-0.0.1-jar-with-dependencies.jar main.Main'
+        }
+    },
+
+    fest: {
+        templates: {
+            files: [{
+                expand: true, // Флаг динамического развертывания
+                cwd: 'templates', /* исходная директория */
+                src: '*.xml', /* имена шаблонов */
+                dest: 'public_html/js/tmpl' /* результирующая директория */
+            }],
+            options: {
+                template: function (data) {
+                    return grunt.template.process(
+                        // 'var <%= name %>Tmpl = <%= contents %> ;',
+                        'define(function () { return <%= contents %> ; });',
+                        {data: data}
+                    );
+                }
             }
         }
-    }
-},
-watch: {
-	fest: { /* Цель */
-    	files: ['templates/*.xml'], /* следим за шаблонами */
-    	tasks: ['fest'], /* перекомпилировать */
-    	options: {
-            interrupt: true,
-        	atBegin: true /* запустить задачу при старте */
-    	}
-	},
-	server: { /* Цель */
-    	files: [
-            'public_html/js/**/*.js', /* следим за статикой */
-            'public_html/css/**/*.css'
-        ],     	
+    },
+
+    sass: {
+        dist: {
+            files: [{
+                expand: true,
+                cwd: 'public_html/css/scss',
+                src: '*.scss',
+                dest: 'public_html/css',
+                ext: '.css'
+            }]
+        }
+    },
+
+    watch: {
+        fest: { /* Цель */
+            files: ['templates/*.xml'], /* следим за шаблонами */
+            tasks: ['fest'], /* перекомпилировать */
+            options: {
+                interrupt: true,
+                atBegin: true /* запустить задачу при старте */
+            }
+        },
+
+        sass: {
+            files: ['public_html/css/scss/*.scss'],
+            tasks: ['sass'],
+            options: {
+                atBegin: true,
+                interrupt: true
+            }
+        },
+
+        server: { /* Цель */
+            files: [
+                'public_html/js/**/*.js', /* следим за статикой */
+                'public_html/css/**/*.css'
+            ],
+            options: {
+                interrupt: true,
+                livereload: true /* автоматическая перезагрузка */
+            }
+        }
+    },
+    concurrent: {
+        target: ['watch', 'shell'],
         options: {
-            interrupt: true,
-        	livereload: true /* автоматическая перезагрузка */
-    	}
-	}
-},
-concurrent: {
-    target: ['watch', 'shell'],
-    options: {
-        logConcurrentOutput: true /* Вывод процесса логов*/
+            logConcurrentOutput: true /* Вывод процесса логов*/
+        }
     }
-}, 
-any_other_name: 'hello' /* Любое произвольное свойство */
+
 });
 
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-concurrent');
-grunt.loadNpmTasks('grunt-shell');
-grunt.loadNpmTasks('grunt-fest');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-fest');
 
-grunt.registerTask('default', ['concurrent']);
+    grunt.registerTask('default', ['concurrent']);
 
 };
