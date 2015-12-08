c301ce1e69ed99a6e9ade87838ed23aa57dc2850
iShoma <shamil.makhmutov@gmail.com>
2015-12-08 14:39:15 +0300

    nice resize

--- public_html/js/lib/shoma.js
+++ public_html/js/lib/shoma.js
@@ -4,7 +4,25 @@
 //     Shoma may be freely distributed under the MIT license.
 //     For all details and documentation:
 //     http://iShoma.com
+
 ;define(function () {
+
+    var tv = $('#tv');
+
+    var tvWidth = 100;
+    var tvHeight = 100;
+
+    function tvSize() {
+        if (windowIsPortr()){
+            tvWidth = windowWidth;
+            tvHeight = tvWidth / 16 * 9;
+        } else {
+            tvHeight =windowHeight;
+            tvWidth= tvHeight * 16 / 9;
+        }
+        tv.width(tvWidth);
+        tv.height(tvHeight);
+    }
 
     $(document).ready(
         resizeEvent()
@@ -15,8 +33,16 @@
     });
 
     function resizeEvent() {
-        $('#tv').width(window.innerWidth);
-        $('#tv').height(window.innerHeight);
+        windowHeight = window.innerHeight;
+        windowWidth = window.innerWidth;
+        tvSize();
     }
+
+    function windowIsPortr() {
+        return windowHeight * 16 / 9 > windowWidth;
+    }
+
+    var windowWidth;
+    var windowHeight;
 
 });
