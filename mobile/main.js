9e0ed91350b26ff494fd089f6e56f9cb5ef94db1
iShoma <shamil.makhmutov@gmail.com>
2015-12-24 16:40:21 +0300

    mobile

---
+++ mobile/main.js
@@ -0,0 +1,52 @@
+/**
+ * Created by shamilmakhmutov on 24.12.15.
+ */
+var Orientation = {
+    PORTRAIT: 0,
+    LANDSCAPE: 1
+};
+
+function resizeEvent(newOrientation) {
+    var MAGIC_NUMBER = 100;
+    var $html = $('html'),
+        fontSize;
+
+    if (newOrientation == Orientation.PORTRAIT) {
+        fontSize = window.innerWidth / MAGIC_NUMBER;
+    } else {
+        fontSize = window.innerHeight / MAGIC_NUMBER;
+    }
+
+    $html.css({
+        'font-size': fontSize
+    });
+}
+
+function redraw(newOrientation) {
+    var $gamepad = $('.gamepad');
+
+    var toPortrait = function () {
+        $gamepad.removeClass('gamepad_landscape').addClass('gamepad_portrait');
+    };
+
+    var toLandscape = function () {
+        $gamepad.removeClass('gamepad_portrait').addClass('gamepad_landscape');
+    };
+
+    if (newOrientation === Orientation.LANDSCAPE) {
+        toLandscape();
+    } else {
+        toPortrait();
+    }
+    resizeEvent(newOrientation);
+}
+
+$(window).on("orientationchange", function (event) {
+    if (event.orientation === 'portrait') {
+        redraw(Orientation.PORTRAIT);
+    } else {
+        redraw(Orientation.LANDSCAPE);
+    }
+});
+
+$(window).trigger("orientationchange");
